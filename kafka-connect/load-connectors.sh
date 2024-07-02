6fa4f6251faa9838b734ed4c035de4bbd602b207
Roman Kudryashov <rskudryashov@gmail.com>
2024-07-02 11:55:22 +0300

    initial commit

---
+++ kafka-connect/load-connectors.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+apk update && apk add curl
+
+KAFKA_CONNECT_URL='http://kafka-connect:8083/connectors'
+
+echo -e "\nWaiting for Kafka Connect to start listening on kafka-connect ⏳"
+while [ $(curl -s -o /dev/null -w %{http_code} $KAFKA_CONNECT_URL) -eq 000 ] ; do
+  echo -e $(date) " Kafka Connect HTTP listener state: "$(curl -s -o /dev/null -w %{http_code} $KAFKA_CONNECT_URL)" (waiting for 200)"
+  sleep 5
+done
+nc -vz kafka-connect 8083
+
+echo -e "\nStart connectors loading"
+
+CONNECTORS=(
+  'book.sink.json'
+  'book.sink.streaming.json'
+  'book.source.json'
+)
+
+for connector in "${CONNECTORS[@]}"
+do
+  echo -e "\n\nCreating a connector: $connector..."
+  while [ $(curl -s -o /dev/null -w %{http_code} -v -H 'Content-Type: application/json' -X POST --data @/usr/connectors/$connector $KAFKA_CONNECT_URL) -ne 201 ] ; do
+    echo -e $(date) " repeat loading '$connector'"
+    sleep 5
+  done
+  echo "Connector '$connector' loaded"
+done
+
+echo -e "\nAll connectors loaded"
